e2ab29186e38a8ccc104a5454a6c9373123d832e
Rachel <rachelmmarriott@gmail.com>
2022-02-26 12:44:25 -0700

    update css styling

--- app.js
+++ app.js
@@ -14,96 +14,187 @@ const promptUser = () => {
             type: 'input',
             name: 'name',
             message: "What is the team manager's name?",
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log("Required");
+                  return false;
+                }
+              }
         },
         {
             type: 'input',
             name: 'id',
             message: "What is the team manager's employee id?",
+            validate: idInput => {
+                if (idInput) {
+                  return true;
+                } else {
+                  console.log("Required");
+                  return false;
+                }
+              }
         },
         {
             type: 'input',
             name: 'email',
             message: "What is the team manager's email address?",
+            validate: emailInput => {
+                if (emailInput) {
+                  return true;
+                } else {
+                  console.log("Required");
+                  return false;
+                }
+              }
         },
         {
             type: 'input',
             name: 'office',
             message: "What is the team manager's office number?",
+            validate: officeInput => {
+                if (officeInput) {
+                  return true;
+                } else {
+                  console.log("Required");
+                  return false;
+                }
+              }
         },
         {
             type: 'list',
             name: 'position',
             message: "Which employee type would you like to add to the team?",
-            choices: ["Engineer", "Intern", "Finished Building Team"]
+            choices: ["Engineer", "Intern", "Finished Building Team"],
         },
-    ]);
-};
-
-const promptPosition = teamData => {
-    console.log(`
----------------------------
- Add Team Member to Roster
----------------------------
-    `);
-
-    return inquirer
-    .prompt([
         {
             type: 'input',
             name: 'engineer-name',
             message: "What is the engineer's name?",
-            // when: ({ position = Engineer }) => addEngineer
+            when: ({ position }) => answers.position === "Engineer"
         },
         {
             type: 'input',
             name: 'engineer-id',
             message: "What is the engineer's employee ID?",
-            // when: ({ position = Engineer }) => addEngineer
+            when: ({ position }) => answers.position === "Engineer"
         },
         {
             type: 'input',
             name: 'engineer-email',
             message: "What is the engineer's email address?",
-            // when: ({ position = Engineer }) => addEngineer
+            when: ({ position }) => answers.position === "Engineer"
         },
         {
             type: 'input',
             name: 'github',
             message: "What is the engineer's GitHub username?",
-            // when: ({ position = Engineer }) => addEngineer
+            when: ({ position }) => answers.position === "Engineer"
         },
         {
             type: 'input',
             name: 'intern-name',
             message: "What is the intern's name?",
-            // when: ({ position = Intern }) => addIntern
+            when: ({ position }) => answers.position === "Intern"
         },
         {
             type: 'input',
             name: 'intern-id',
             message: "What is the intern's employee ID?",
-            // when: ({ position = Intern }) => addIntern
+            when: ({ position }) => answers.position === "Intern"
         },
         {
             type: 'input',
             name: 'intern-email',
             message: "What is the intern's email address?",
-            // when: ({ position = Intern }) => addIntern
+            when: ({ position }) => answers.position === "Intern"
         },
         {
             type: 'input',
             name: 'school',
             message: "What is the intern's school?",
-           //  when: ({ position = Intern }) => addIntern
+            when: ({ position }) => answers.position === "Intern"
         },
-    ])   
-}
+    ]);
+};
 
+// const promptPosition = teamData => {
+//     console.log(`
+// ---------------------------
+//  Add Team Member to Roster
+// ---------------------------
+//     `);
+
+//     return inquirer
+//     .prompt([
+//         {
+//             type: 'input',
+//             name: 'engineer-name',
+//             message: "What is the engineer's name?",
+//             // when: ({ position = Engineer }) => addEngineer
+//         },
+//         {
+//             type: 'input',
+//             name: 'engineer-id',
+//             message: "What is the engineer's employee ID?",
+//             // when: ({ position = Engineer }) => addEngineer
+//         },
+//         {
+//             type: 'input',
+//             name: 'engineer-email',
+//             message: "What is the engineer's email address?",
+//             // when: ({ position = Engineer }) => addEngineer
+//         },
+//         {
+//             type: 'input',
+//             name: 'github',
+//             message: "What is the engineer's GitHub username?",
+//             // when: ({ position = Engineer }) => addEngineer
+//         },
+//         {
+//             type: 'input',
+//             name: 'intern-name',
+//             message: "What is the intern's name?",
+//             // when: ({ position = Intern }) => addIntern
+//         },
+//         {
+//             type: 'input',
+//             name: 'intern-id',
+//             message: "What is the intern's employee ID?",
+//             // when: ({ position = Intern }) => addIntern
+//         },
+//         {
+//             type: 'input',
+//             name: 'intern-email',
+//             message: "What is the intern's email address?",
+//             // when: ({ position = Intern }) => addIntern
+//         },
+//         {
+//             type: 'input',
+//             name: 'school',
+//             message: "What is the intern's school?",
+//            //  when: ({ position = Intern }) => addIntern
+//         },
+//     ])   
+// }
+
+
+// function call to begin app
 promptUser()
-.then(promptPosition)
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// send answers to file template
+.then(employeeData => {
+    return generateTeamRoster(employeeData);
+})
 
-    console.log('Team Roster complete!  Check out index.html to see the output!');
-});
+// write the team roster file and insert in 'dist' folder
+.then(teamRoster => {
+    console.log(teamRoster);
+    return writeFile(teamRoster);
+})
+
+  // catch errors
+  .catch(err => {
+    console.log(err);
+  });
